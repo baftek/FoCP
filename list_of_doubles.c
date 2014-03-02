be587a50bef05d0933457515d4b4d4073ea18e3e
Siarkowy <siarkowy@siarkowy.net>
2014-03-03 00:53:55 +0100

    Laboratory: Introduction

---
+++ list_of_doubles.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILE_NAME "data.txt"
+
+typedef struct Data
+{
+    double value;
+    struct Data *next;
+} Data;
+
+int main()
+{
+    FILE* input = NULL;
+    Data* head = NULL;
+    Data* current = NULL;
+
+    // Opening of the file
+    if (!(input = fopen(FILE_NAME, "r")))
+    {
+        fprintf(stderr, "Could not open input file.");
+        return 1;
+    }
+
+    // Reading from file
+    do
+    {
+        double value;
+
+        if (fscanf(input, "%lf", &value) == 1)
+        {
+            current = (Data *) malloc(sizeof(*current));
+
+            if (!current)
+            {
+                fprintf(stderr, "Out of memory.");
+                return 2;
+            }
+
+            current->value = value;
+            current->next = head;
+            head = current;
+        }
+    } while (!feof(input));
+
+    // Printing
+    for (current = head; current != NULL; current = current->next)
+        printf("%.2lf\n", current->value);
+
+    // Memory freeing
+    while (head)
+    {
+        current = head->next;
+        free(head);
+        head = current;
+    }
+
+    return 0;
+}
